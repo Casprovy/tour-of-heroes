326e941d8fc883cc5d85714789a3a34c31521156
Tomasz Gasienica-Szostak <casprovy.tgs@gmail.com>
2020-02-06 22:15:52 +0100

    adding routing

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from "@angular/core";
+import { Observable, of } from "rxjs";
+
+import { MessageService } from "./message.service";
 
 import { Hero } from "./hero";
 import { HEROES } from "./mock-heroes";
@@ -7,9 +10,10 @@ import { HEROES } from "./mock-heroes";
   providedIn: "root"
 })
 export class HeroService {
-  constructor() {}
+  constructor(private messageService: MessageService) {}
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add("HeroService: fetched heroes");
+    return of(HEROES);
   }
 }
